9b7eb1062c0392e55f22541b9f66e1ff931e4570
Rasul Kireev <rasul.kireev@guycarp.com>
2023-07-20 10:51:23 -0300

    run schedule daily

--- users/schedules.py
+++ users/schedules.py
@@ -6,6 +6,7 @@ schedules = [
         "func_path": "users.tasks.find_subs_to_alert",
         "hook": "jobs.hooks.print_result",
         "args": None,
-        "type": Schedule.MINUTES,
+        # "type": Schedule.MINUTES,
+        "type": Schedule.DAILY,
     },
 ]
