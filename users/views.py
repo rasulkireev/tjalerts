b2cb70f6577f77e12d282a4a9dfd9ebdeabef7ba
Rasul Kireev <me@rasulkireev.com>
2023-11-17 11:44:08 -0500

    only allow users to use legit tech names

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from allauth.account.utils import send_email_confirmation
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, UpdateView
@@ -12,10 +13,16 @@ from django_q.tasks import async_task
 from hn_jobs.utils import add_users_context
 
 from .forms import CreateAlertForm, UpdateAlertForm
-from .models import CustomUser, Subscriber
+from .models import CustomUser, Subscriber, Technology
 from .tasks import find_subs_to_alert, send_confirmation_email
 
 logger = logging.getLogger(__file__)
+
+
+def validate_technology_selected(value):
+    technologies = Technology.objects.values_list("name", flat=True)
+    if value not in technologies:
+        raise ValidationError(f"{value} is not a valid technology name.")
 
 
 class UserSettingsView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
@@ -53,6 +60,12 @@ class AlertCreateView(SuccessMessageMixin, CreateView):
     success_message = "Thanks for subscribing :) Check your emails to confirm!"
 
     def form_valid(self, form):
+        try:
+            validate_technology_selected(form.cleaned_data["technology_selected"])
+        except ValidationError:
+            messages.add_message(self.request, messages.WARNING, "Please use a Technology from the dropdown list.")
+            return redirect("home")
+
         if Subscriber.objects.filter(email=form.instance.email).exists():
             messages.add_message(self.request, messages.WARNING, "An alert already exists for this email.")
             return redirect("home")
