c4e9f979109d36949d2104bc26ad56a6e0ffc984
Rasul Kireev <me@rasulkireev.com>
2024-04-19 09:47:47 +0300

    add remote

--- api/views.py
+++ api/views.py
@@ -88,6 +88,7 @@ def get_jobs(request, technologies=Query(None)):
             "compensation_summary": post.compensation_summary,
             "min_salary": post.min_salary,
             "max_salary": post.max_salary,
+            "is_remote": post.is_remote,
             "technologies": post_technologies,
             "title": post_titles,
             "id": str(post.id),
