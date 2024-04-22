695e05c230707ca993496b72535afc0ba4cadb54
Rasul Kireev <me@rasulkireev.com>
2024-04-22 22:03:11 +0300

    trans

--- hn_jobs/settings/production.py
+++ hn_jobs/settings/production.py
@@ -2,6 +2,6 @@
 
 from .base import *
 
-LOGGING["loggers"]["hn_jobs"]["level"] = env("DJANGO_LOG_LEVEL", default="INFO")
-LOGGING["loggers"]["hn_jobs"]["handlers"] = ["json_console"]
+LOGGING["loggers"]["tjalerts"]["level"] = env("DJANGO_LOG_LEVEL", default="INFO")
+LOGGING["loggers"]["tjalerts"]["handlers"] = ["json_console"]
 LOGGING["loggers"]["django_structlog"]["handlers"] = ["json_console"]
