c26ab173e87c261130eac2e1764aacebec532284
Rasul Kireev <me@rasulkireev.com>
2024-06-07 12:49:09 +0300

    prd setting

--- hn_jobs/asgi.py
+++ hn_jobs/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hn_jobs.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hn_jobs.settings.prod")
 
 application = get_asgi_application()
--- hn_jobs/wsgi.py
+++ hn_jobs/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-# import os
+import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hn_jobs.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hn_jobs.settings.prod")
 
 application = get_wsgi_application()
