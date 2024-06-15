deb2d1acf9a2eaaaecf345c805195d7f029eab71
Rasul Kireev <me@rasulkireev.com>
2024-06-15 10:11:27 +0300

    add tqdm

--- utils/scripts.py
+++ utils/scripts.py
@@ -1,12 +1,17 @@
+from tqdm import tqdm
+
 from jobs.models import Technology, TechnologyMapping
 from utils.constants import HIRABLE_TECH_LIST_MAPPING
 
-for pair in HIRABLE_TECH_LIST_MAPPING:
-    print(pair)
+with tqdm(total=len(HIRABLE_TECH_LIST_MAPPING)) as pbar:
+    for pair in HIRABLE_TECH_LIST_MAPPING:
+        tqdm.write(f"{pair}")
 
-    try:
-        parent = Technology.objects.get(id=pair[0])
-        child = Technology.objects.get(id=pair[1])
-        TechnologyMapping.objects.get_or_create(parent=parent, child=child)
-    except Technology.DoesNotExist:
-        pass
+        try:
+            parent = Technology.objects.get(id=pair[0])
+            child = Technology.objects.get(id=pair[1])
+            TechnologyMapping.objects.get_or_create(parent=parent, child=child)
+        except Technology.DoesNotExist:
+            pass
+
+        pbar.update(1)
